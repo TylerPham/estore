493086cd3a503acf359a68394d49eee704fd2743
ExGale <steven_vetson@hotmail.com>
2014-11-18 23:12:44 -0500

    Autoloader and created the remaining view files

--- estore/application/config/autoload.php
+++ estore/application/config/autoload.php
@@ -112,7 +112,7 @@ $autoload['language'] = array();
 */
 
 //$autoload['model'] = array('MCats','MProducts','MOrders','MAdmins','MSizes','MColors', 'MPages', 'MSubscribers');
-$autoload['model'] = array('Product', 'newUser', 'loginInfo', 'orders');
+$autoload['model'] = array('Product', 'Order', 'OrderItem', 'Customer');
 
 
 /* End of file autoload.php */
